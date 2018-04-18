afb1e7a70e60d394cf5986baf6120b874d83c4fd
Amy Sitwala <asitwala17@gmail.com>
2018-04-17 20:04:55 -0500

    Committing updates on stepper

--- src/demo-user/user-info/InitializeStep3.vue
+++ src/demo-user/user-info/InitializeStep3.vue
@@ -1,23 +1,39 @@
 <template>
     <div class="as-initialize-step-3">
+        <h2>You've been placed at LEVEL 1</h2>
         <h2>You're almost there! All that's left is to select your workout days.</h2>
 
-        <h3>Select a start date</h3>
-        <h3>Select 3 days you'd like to work out on</h3>
-        
-        <div class="as-initialize-step-3-days">
-            <div class="as-initialize-step-3-day">
-                <v-checkbox v-for="day in days" :key="day"
-                    color="primary"
-                    :label="day" v-model="selectedDays" :value="day">
-                </v-checkbox>
+        <div class="as-initialize-step-3-container">
+            <div class="as-initialize-step-3-left">
+                <h3>Select a start date</h3>
+                <p style="margin-bottom: 0px !important;">Enter below or use by clicking on the calendar to the right.</p>
+                <v-form v-model="validForm">
+                    <v-text-field
+                        v-model="startDate"
+                        :rules="startDateRules"
+                        return-masked-value
+                        placeholder="MM/DD/YYYY"
+                        mask="##/##/####"
+                    />
+                </v-form>
+
+                <h3 class="as-initialize-step-3-select-days">Select 3 days you'd like to work out on</h3>
+                
+                <div class="as-initialize-step-3-days">
+                    <div class="as-initialize-step-3-day"
+                        v-for="day in days" :key="day">
+                        <v-checkbox 
+                            color="primary"
+                            :label="day" v-model="selectedDays" :value="day">
+                        </v-checkbox>
+                    </div>
+                </div>
+            </div>
+            <div class="as-initialize-step-3-right">
+                <v-date-picker v-model="startDateCalendar"></v-date-picker>
             </div>
         </div>
-    
-        <v-date-picker v-model="startDate"></v-date-picker>
-
     </div>
-
 </template>
 
 <script>
@@ -27,7 +43,42 @@
             return {
                 days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
                 selectedDays: [], 
-                startDate: moment().format(`YYYY-MM-DD`)
+                startDateCalendar: moment().format(`YYYY-MM-DD`),
+                startDate: moment().format(`MM/DD/YYYY`),
+                startDateRules: [
+                    v => moment(v, 'MM/DD/YYYY', true).isValid() || 'Please use a valid date format.'
+                ],
+                validForm: false
+            }
+        },
+        methods: {
+            supportCalendar(textValue) {
+                let dateKeys = textValue.split('/');  // YYYY, MM, DD
+                let month = dateKeys[0]; 
+                let day = dateKeys[1]; 
+                let year = dateKeys[2]; 
+                this.startDateCalendar = `${year}-${month}-${day}`;
+            },
+            supportText(calendarValue) {
+                let dateKeys = calendarValue.split('-');  // YYYY, MM, DD
+                let month = dateKeys[1]; 
+                let day = dateKeys[2]; 
+                let year = dateKeys[0]; 
+                this.startDate = `${month}/${day}/${year}`;
+            },
+            calculateDays() {
+                //moment(this.startdate, "YYYY-MM-DD").add('days', 5);
+                //
+            }
+        },
+        watch: {
+            startDate: function(newValue) {
+                if (moment(newValue, 'MM/DD/YYYY', true).isValid()) {
+                    this.supportCalendar(newValue); 
+                }
+            },
+            startDateCalendar: function(newValue) {
+                this.supportText(newValue); 
             }
         }
     };
@@ -35,13 +86,39 @@
 
 <style lang="scss">
 
+    .as-initialize-step-3 {
+        h2:nth-of-type(2) {
+            margin-bottom: 16px; 
+        }
+    }
+
+    .as-initialize-step-3-select-days {
+        padding-bottom: 16px;
+    }
+
     .as-initialize-step-3-days {
         display: flex; 
         flex-wrap: wrap; 
     }
 
     .as-initialize-step-3-day {
-        display: inline-block;
+        display: block !important;
+        width: 150px;
+        min-width: 150px;
+    }
+
+    .as-initialize-step-3-container {
+        display: flex; 
+        flex-wrap: wrap;
+
+        .as-initialize-step-3-left {
+            flex: 1; 
+            margin-right: 20px;
+        }
+
+        .as-initialize-step-3-right {
+            width: 300px;
+        }
     }
 
 </style>
