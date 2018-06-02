4bbd9f3a423c76cfa922f93768a4198cea8cc2e0
Amy Sitwala <asitwala17@gmail.com>
2018-06-02 12:51:22 -0500

    Committing latest changeds for sanity checks on be

---
+++ src/demo-user/CheckEmail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="as-check-email">
+        <h2></h2>
+    </div>
+
+
+</template>
+
+
+<script>
+
+
+</script>
+
+
+<style lang="scss">
+    .as-check-email {
+        display: flex; 
+        align-items: center;
+    }   
+
+</style>
