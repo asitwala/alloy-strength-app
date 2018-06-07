6e05d9e5aa23a04d40210f6d86cd4b5550c33503
Amy Sitwala <asitwala17@gmail.com>
2018-06-06 23:02:24 -0500

    Removing psd, ai, and old about photos

---
+++ src/demo-common/components/SetWarning.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="as-set-warning">
+        <v-dialog v-model="showDialog" max-width="290" persistent>
+            <v-card>
+            <v-card-text>{{ dialogText }}</v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" flat="flat" @click.native="closeWarning">Ok</v-btn>
+            </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    props: {
+        showWarning: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        closeWarning() {
+            this.$emit('close');
+        }
+    }
+};
+
+</script>
+
+
+<style lang="scss">
+
+
+</style>
